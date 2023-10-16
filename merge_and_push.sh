f97016118d6b568dd0fa28bb540683df316d4f2a
Nuno <nuno313@gmail.com>
2023-10-16 15:45:53 +0100

    change mer.push

---
+++ merge_and_push.sh
@@ -0,0 +1,20 @@
+git add .
+
+echo 'Commit message:'
+read Message
+
+git commit -am "$Message"
+
+echo 'Target branch:'
+read target_branch
+
+git checkout $target_branch
+
+echo "Merging into $target_branch"
+
+
+git push origin $target_branch
+
+echo "Changes have been merged and pushed to $target_branch"
+
+read
