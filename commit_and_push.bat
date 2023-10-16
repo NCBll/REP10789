0e9e6b330d92a12672d6e75a74e5339b85418f8e
Nuno <nuno313@gmail.com>
2023-10-16 12:36:43 +0100

    deleted

--- commit_and_push.bat
+++
@@ -1,8 +0,0 @@
-pushd "%userprofile%\Documents\dotNet_Formação\Git e Gituhb\REP10789
-echo.>.gitignore
-bash -c "command find . -type f -size +99M >> .gitignore"
-bash -c "sed -i "s,^\./,," .gitignore" 
-git add --all
-git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-git push
-popd
