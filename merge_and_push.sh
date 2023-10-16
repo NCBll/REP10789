44f5e7f46ad2b84efb06b89a1f61cdcdd1453271
Nuno <nuno313@gmail.com>
2023-10-16 15:38:09 +0100

    "try"

--- merge_and_push.sh
+++ merge_and_push.sh
@@ -4,6 +4,9 @@ echo 'Commit message:'
 read Message
 
 git commit -am "$Message"
+echo 'current Branch:'
+read current_Branch
+git merge $current_Branch
 
 echo 'Target branch:'
 read target_branch
@@ -11,8 +14,8 @@ read target_branch
 git checkout $target_branch
 
 echo "Merging into $target_branch"
-git merge source_branch
+
 
 git push origin $target_branch
 
-echo "Changes have been merged and pushed to $target_branch."
+echo "Changes have been merged and pushed to $target_branch
